34709fc6d720ccfe63a474b29061bf7d0bcb60e5
Paludeto <gpalu54@gmail.com>
2025-04-11 17:37:44 -0300

    classe luta, teste de luta

--- ufc/ufc.cpp
+++ ufc/ufc.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
@@ -44,6 +46,7 @@ class Lutador {
         int getEmpates() const { return empates; }
         float getAltura() const { return altura; }
         float getPeso() const { return peso; }
+        bool getStatus() const { return ativo; }
 
         // Setters
         void setNome(const string& nome) { this->nome = nome; }
@@ -55,6 +58,7 @@ class Lutador {
         void setEmpates(int empates) { this->empates = empates; }
         void setAltura(float altura) { this->altura = altura; }
         void setPeso(float peso) { this->peso = peso; }
+        void setStatus(bool ativo) { this->ativo = ativo; }
 
         // Outros métodos
         void apresentar() {
@@ -86,6 +90,84 @@ class Lutador {
 
 };
 
+class Luta {
+
+    private:
+
+        Lutador *desafiado, *desafiador;
+        int rounds;
+        bool aprovada;
+
+    public:
+
+        Luta(Lutador *desafiado, Lutador *desafiador, int rounds) {
+            this->desafiado = desafiado;
+            this->desafiador = desafiador;
+            this->rounds = rounds;
+            this->aprovada = false;
+
+            marcarLuta();
+
+            if (aprovada) {
+                lutar();
+            } else {
+                return;
+            }
+            
+        };
+
+        ~Luta() {
+            cout << "Luta destruída" << endl;
+        }
+
+        void setAprovacao(bool aprovada) {
+            this->aprovada = aprovada;
+        }
+
+        void marcarLuta() {
+
+            if ((desafiado->getCategoria() == desafiador->getCategoria()) && (desafiado->getStatus() && desafiador->getStatus())) {
+                cout << "Luta marcada!" << endl;
+                setAprovacao(true);
+            } else {
+                cout << "Luta negada!" << endl;
+                setAprovacao(false);
+            }
+
+        }
+
+        void lutar() {
+
+            srand(time(NULL));
+
+            int estado = rand() % 3;
+
+            switch (estado) {
+
+                case 0:
+                    this->desafiado->ganharLuta();
+                    this->desafiador->perderLuta();
+                    cout << "Desafiado " << desafiado->getNome() << " venceu!" << endl;
+                    break;
+                case 1:
+                    this->desafiador->ganharLuta();
+                    this->desafiado->perderLuta();
+                    cout << "Desafiador " << desafiador->getNome() << " venceu!" << endl;
+                    break;
+                case 2:
+                    this->desafiado->empatarLuta();
+                    this->desafiador->empatarLuta();
+                    cout << "Empate!" << endl;
+                    break;
+                default:
+                    break;
+
+            }
+
+        }
+
+};
+
 int main(void) {
 
     vector<Lutador *> lutadores;
@@ -99,6 +181,10 @@ int main(void) {
     for (Lutador *l : lutadores) {
         l->apresentar();
     }
+
+    Luta *luta_1 = new Luta(lutadores.at(0), lutadores.at(1), 4);
+
+    delete(luta_1);
 
     for (Lutador *l : lutadores) {
         delete(l);
